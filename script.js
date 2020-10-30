5ce782de47b4a38858c7e6b3842d539b70318819
Zyanvel <zyanvel@gmail.com>
2020-10-30 16:28:50 -0700

    buttons first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let container = document.querySelector('#container');
+
+for (let repeat = 0; repeat < 100; repeat++) {
+    const buttons = document.createElement('button');
+    buttons.innerText = 'Doeeeeeey! xD';
+    container.appendChild(buttons);
+};
